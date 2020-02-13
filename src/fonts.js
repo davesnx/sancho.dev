5eb0cf129a235a61251562a4f065438ed5201457
David Sancho <dsnxmoreno@gmail.com>
2020-02-13 20:52:47 +0100

    feat: Finish font-size experiment relative

--- src/fonts.js
+++ src/fonts.js
@@ -1,19 +1,21 @@
-const increment = "1.2";
+const increment = "1.45";
 const lineHeightFixed = "0.5rem";
-const lineHeightRelative = "1.2rem";
+const lineHeightRelative = "1.5rem";
 
-const fontSize0 = "(1rem + 0.15vw)";
-const fontSize1 = `calc(${fontSize0} * ${increment})`;
-const fontSize2 = `calc(${fontSize0} * ${increment} * 2)`;
-const fontSize3 = `calc(${fontSize0} * 3)`;
-const globalFontSize = `${fontSize0}`;
+const fontSizeXS = "0.8rem + 0.15vw";
+const fontSizeS = `(${fontSizeXS}) * ${increment}`;
+const fontSizeM = `(${fontSizeS}) * ${increment}`;
+const fontSizeL = `(${fontSizeM}) * ${increment}`;
+
+const calc = str => `calc(${str})`;
+const globalFontSize = calc(fontSizeXS);
 const globalLineHeight = `calc(${lineHeightFixed} + ${lineHeightRelative})`;
 
 export default {
-  fontSize0,
-  fontSize1,
-  fontSize2,
-  fontSize3,
+  fontSize0: calc(fontSizeXS),
+  fontSize1: calc(fontSizeS),
+  fontSize2: calc(fontSizeM),
+  fontSize3: calc(fontSizeL),
   globalFontSize,
   globalLineHeight
 };
