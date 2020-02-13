90d3c2e1d16e625ad30084b4c2a6ce884c340299
David Sancho <dsnxmoreno@gmail.com>
2020-02-13 20:41:35 +0100

    feat: Finish font-size experiment

--- src/fonts.js
+++ src/fonts.js
@@ -2,10 +2,10 @@ const increment = "1.2";
 const lineHeightFixed = "0.5rem";
 const lineHeightRelative = "1.2rem";
 
-const fontSize0 = "1.2rem";
+const fontSize0 = "(1rem + 0.15vw)";
 const fontSize1 = `calc(${fontSize0} * ${increment})`;
-const fontSize2 = `calc(${fontSize0} * ${increment} * 1.5)`;
-const fontSize3 = `calc(${fontSize0} * ${increment} * 2.5)`;
+const fontSize2 = `calc(${fontSize0} * ${increment} * 2)`;
+const fontSize3 = `calc(${fontSize0} * 3)`;
 const globalFontSize = `${fontSize0}`;
 const globalLineHeight = `calc(${lineHeightFixed} + ${lineHeightRelative})`;
 
