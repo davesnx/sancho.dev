9e1ff6d44a4b3155ccd09043cfa1e4772af055dd
David Sancho <dsnxmoreno@gmail.com>
2020-04-19 15:42:32 +0200

    Fix deploy and add 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Layout from "../components/layout";
+import Main from "../components/main";
+import { H1 } from "../components/heading";
+import Text from "../components/text";
+import Spacer from "../components/spacer";
+import Link from "../components/link";
 
 export default () => (
   <Layout>
-    <h1>
-      <span role="img" aria-label="Ghost">
-        🤔 Not found
-      </span>
-    </h1>
-    <p>
-      It seems this page does not exist. Go back to the{" "}
-      <a href="https://sancho.dev">home page</a>
-    </p>
+    <Main>
+      <Spacer top={12}>
+        <H1>
+          <span role="img" aria-label="Ghost">
+            🤔 Not found
+          </span>
+        </H1>
+        <Text>
+          It seems this page does not exist. Go back to{" "}
+          <Link href="https://sancho.dev">home</Link>
+        </Text>
+      </Spacer>
+    </Main>
   </Layout>
 );
