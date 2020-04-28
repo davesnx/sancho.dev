8de9c7e7fddd2e475a9a349e0053ff4e533c355c
David Sancho <dsnxmoreno@gmail.com>
2020-04-28 23:44:30 +0200

    Remove useless h1

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../components/layout";
 import Spacer, { RelativeSpacer } from "../components/spacer";
 import Main from "../components/main";
-import { H1 } from "../components/heading";
 
 const Page = ({ title, children }) => (
   <Layout>
