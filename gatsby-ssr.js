31e243223d44e7c594851f3a8ef2b66ed888fb9e
David Sancho <dsnxmoreno@gmail.com>
2020-05-25 14:55:17 +0200

    fix: gatsby flick styles v4

---
+++ gatsby-ssr.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyles from "./src/components/global-styles";
+import "./static/fonts/styles.css";
+
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return (
+    <>
+      <GlobalStyles />
+      {element}
+    </>
+  );
+};
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import GlobalStyles from "./global-styles";
 import SiteMetadata from "./site-metadata";
 import colors from "./colors";
-import "../../static/fonts/styles.css";
 
 const Root = styled.div`
   min-width: 100vw;
@@ -14,7 +12,6 @@ const Root = styled.div`
 export default ({ children, pathname, backgroundColor = colors.white }) => {
   return (
     <>
-      <GlobalStyles />
       <Root backgroundColor={backgroundColor}>
         <SiteMetadata pathname={pathname} />
         {children}
