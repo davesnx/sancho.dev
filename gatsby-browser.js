6dac4cf27e03bb9b74cd126967fddcc438ae6579
David Sancho <dsnxmoreno@gmail.com>
2020-05-25 16:29:22 +0200

    revert: fix: gatsby flick styles v7

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import GlobalStyles from "./src/components/global-styles";
 
 import "./static/fonts/styles.css";
 
-export const wrapRootElement = ({ element }) => {
+export const wrapPageElement = ({ element }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return (
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from "react";
 import GlobalStyles from "./src/components/global-styles";
 import "./static/fonts/styles.css";
 
-export const wrapRootElement = ({ element }) => {
+export const wrapPageElement = ({ element }) => {
   return (
     <>
       <GlobalStyles />
