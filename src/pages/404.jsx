09e8dd0015a72ef9be56809eca9a14054e0b9cb4
David Sancho <dsnxmoreno@gmail.com>
2021-01-12 14:12:32 +0100

    feat: Improve 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import font from "../components/fonts";
 
 import Text from "../components/text";
 import Page from "../components/page";
@@ -6,15 +8,19 @@ import Spacer from "../components/spacer";
 import Navigate from "../components/navigate";
 import Link from "../components/link";
 
+const Small = styled(Text)`
+  font-size: ${font.fontSizeN1};
+`;
+
 export default () => (
-  <Page title="Page cannot be found">
-    <Text>
-      If it sounds like it shoud 🤔, please open an issue{" "}
-      <Link to="https://github.com/davesnx/sancho.dev">here</Link>.
+  <Page title="Page not found">
+    <Text weigth={600}>
+      If it sounds like it shoud, <br /> please open an issue in{" "}
+      <Link to="https://github.com/davesnx/sancho.dev">the repo</Link>.
     </Text>
-    <Spacer top={2} />
-    <Text>
+    <Spacer top={4} />
+    <Small>
       Go back to <Navigate to="https://sancho.dev">home</Navigate>.
-    </Text>
+    </Small>
   </Page>
 );
