fc1be0ebdd098c49be737ee3af944d1cef7141ee
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-02-14 21:18:58 +0100

    breakline on the index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,14 +11,26 @@ import SocialMedia from "../components/social-media";
 export default () => {
   return (
     <Page title="Hey! I'm David Sancho">
-      <Text align="left">
-        Software engineer based in Barcelona. Trying to make cute software with{" "}
+      <Text align="left">Software engineer based in Barcelona. </Text>
+      <Spacer top={2} />
+      <Text>
+        Trying to make cute software with{" "}
         <Link to="http://reasonml.github.io/">Reason</Link> and{" "}
-        <Link to="https://ocaml.org/">OCaml</Link>. I co-host <Link to="https://www.twitch.tv/emelletv">EmelleTV</Link> a stream show about those languages. </Text>
+        <Link to="https://ocaml.org/">OCaml</Link>. I co-host{" "}
+        <Link to="https://www.twitch.tv/emelletv">EmelleTV</Link> a stream show
+        about those languages.{" "}
+      </Text>
       <Spacer top={2} />
-      <Text>In my spare time, I train as an amateur triathlete focused in Olympic and Half Ironman distances and creating/maintaing a bunch of Open Source projects</Text>
+      <Text>
+        In my spare time, I train as an amateur triathlete focused in Olympic
+        and Half Ironman distances and creating/maintaing a bunch of Open Source
+        projects
+      </Text>
       <Spacer top={2} />
-      <Text>You can read more about me in the <Navigate to="/about">/about</Navigate> page.</Text>
+      <Text>
+        You can read more about me in the{" "}
+        <Navigate to="/about">/about</Navigate> page.
+      </Text>
       <Spacer top={4} />
       <SocialMedia />
     </Page>
