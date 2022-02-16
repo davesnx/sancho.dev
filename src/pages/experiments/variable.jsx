3d681304cad5ece6fa0567c9f21629a78c9a6480
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-02-16 09:19:25 +0100

    fix: Link on variable font

--- src/pages/experiments/variable.jsx
+++ src/pages/experiments/variable.jsx
@@ -80,9 +80,8 @@ export default () => {
             {isMobile
               ? "Tap into the name to see the "
               : "Move the mouse in the y axis to see the "}
-            <Link to="https://en.wikipedia.org/wiki/Chromatic_aberration">
-              Variable font weight effect
-            </Link>
+            <Link to="https://v-fonts.com">Variable font weight</Link>
+            effect
           </Text>
         </Spacer>
       </Container>
