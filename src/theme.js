25ba38e9db93573a516a811919874346914f4ab4
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-03-04 00:03:01 +0100

    feat: Change github color

--- src/theme.js
+++ src/theme.js
@@ -22,7 +22,7 @@ const darkValues = {
   subtle: "#54585d",
   primary: "#3a80bf",
   twitter: "rgba(29, 161, 242, 1)",
-  github: "rgba(24, 23, 23, 1)",
+  github: "rgba(164, 173, 187, 1)",
   telegram: "rgba(114, 137, 218, 1)",
   strava: "rgba(252, 76, 2, 1)",
   r, g, b
