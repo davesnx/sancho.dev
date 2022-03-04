8fe25f735721674c8f923ad60d73a1deaa8baed9
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-03-04 11:43:38 +0100

    fix mobile

--- src/utils/media-query.js
+++ src/utils/media-query.js
@@ -3,7 +3,7 @@ import useMedia from "use-media";
 import constants from "../constants";
 
 export const useIsMobile = () => {
-  const isWide = useMedia({ minWidth: constants.width });
+  const isWide = useMedia({ minWidth: constants.mobile.width });
   const isSmall = !isWide;
   return isSmall;
 };
