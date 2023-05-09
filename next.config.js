43b66a2af2aac2cbc0824f52442e4acc4b22a901
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-05-09 18:30:01 +0200

    Compress on

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  */
 
 const nextConfig = {
-  compress: false,
+  compress: true,
   swcMinify: true,
   typescript: {
     // Dangerously allow production builds to successfully complete even if your project has type errors.
