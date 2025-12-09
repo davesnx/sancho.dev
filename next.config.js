961413f520b580c91eb7d1979d8942ece60f8717
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-12-09 14:27:17 +0100

    Fix export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
  */
 
 const nextConfig = {
+  output: 'export',
   compress: true,
   typescript: {
     // Dangerously allow production builds to successfully complete even if your project has type errors.
